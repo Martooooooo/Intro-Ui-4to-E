756b09453c1b3b4456ccd7b5a552dd1e99b643c8
Martooooooo <166547016+Martooooooo@users.noreply.github.com>
2025-04-23 15:38:59 -0300

    Ingreso Clave terminado

---
+++ Assets/ClaveManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ClaveManager : MonoBehaviour
+{
+    public TMP_InputField inputClave;
+    public TextMeshProUGUI txtMensaje;
+
+    string clave = "tic2025";
+    // Start is called before the first frame update
+    void Start()
+    {
+       txtMensaje.text= "";
+    }
+
+    public void ComprobarClaveIngresada()
+    {
+        string ClaveIngresada = inputClave.text;
+        if (ClaveIngresada != clave)
+        {
+            txtMensaje.text = "Acceso denegado";
+            inputClave.text = "";
+        }
+        else
+        {
+            txtMensaje.text = "Bienvenido";
+        }
+    }
+}
